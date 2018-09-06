5aeb92bb016ce287de735121b25c8b9e2e7da271
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-06 13:46:19 +0100

    Add youtube player view programmatically

--- app/src/main/java/app/a2ms/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/app/a2ms/youtubeplayer/YoutubeActivity.kt
@@ -1,15 +1,34 @@
 package app.a2ms.youtubeplayer
 
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.view.ViewGroup
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
+import com.google.android.youtube.player.YouTubePlayerView
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
 
+    @SuppressLint("InflateParams", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_youtube)
+//        setContentView(R.layout.activity_youtube)
+//        val Layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
+        val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
+        setContentView(layout)
+//
+//        val button1 = Button(this)
+//        button1.layoutParams = ConstraintLayout.LayoutParams(600, 180) as ViewGroup.LayoutParams?
+//        button1.text = "Button added"
+//        layout.addView(button1)
+
+        val playerView = YouTubePlayerView(this)
+        playerView.layoutParams = ConstraintLayout.LayoutParams(
+                ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
+        )
+        layout.addView(playerView)
     }
 
     override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
