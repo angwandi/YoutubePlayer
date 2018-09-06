0875d9da3573e4f1afe2c98a6bd27ea1f787ba06
Innocent <31923567+angwandi@users.noreply.github.com>
2018-09-06 15:19:51 +0100

    Add constrictors code

--- app/src/main/java/app/a2ms/youtubeplayer/YoutubeActivity.kt
+++ app/src/main/java/app/a2ms/youtubeplayer/YoutubeActivity.kt
@@ -3,7 +3,9 @@ package app.a2ms.youtubeplayer
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.support.constraint.ConstraintLayout
+import android.util.Log
 import android.view.ViewGroup
+import android.widget.Toast
 import com.google.android.youtube.player.YouTubeBaseActivity
 import com.google.android.youtube.player.YouTubeInitializationResult
 import com.google.android.youtube.player.YouTubePlayer
@@ -13,32 +15,44 @@ const val YOUTUBE_VIDEO_ID = "watch?v=lW2PI6jqn1U"
 const val YOUTUBE_PLAYLIST = "watch?v=8JnfIa84TnU&list=PL4o29bINVT4EG_y-k5jGoOu3-Am8Nvi10"
 
 class YoutubeActivity : YouTubeBaseActivity(), YouTubePlayer.OnInitializedListener {
-
+    private val mTAG = "Youtube Activity"
     @SuppressLint("InflateParams", "SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_youtube)
-//        val Layout = findViewById<ConstraintLayout>(R.id.activity_youtube)
+
         val layout = layoutInflater.inflate(R.layout.activity_youtube, null) as ConstraintLayout
         setContentView(layout)
-//
-//        val button1 = Button(this)
-//        button1.layoutParams = ConstraintLayout.LayoutParams(600, 180) as ViewGroup.LayoutParams?
-//        button1.text = "Button added"
-//        layout.addView(button1)
 
+        //Add playerView programmatically
         val playerView = YouTubePlayerView(this)
         playerView.layoutParams = ConstraintLayout.LayoutParams(
                 ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT
         )
         layout.addView(playerView)
+
+        //initializing the playerView
+        playerView.initialize(getString(R.string.GOOGLE_API_KEY),this)
     }
 
-    override fun onInitializationSuccess(p0: YouTubePlayer.Provider?, p1: YouTubePlayer?, p2: Boolean) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onInitializationSuccess(provider: YouTubePlayer.Provider?, youTubePlayer: YouTubePlayer?, wasRestored: Boolean) {
+        Log.d(mTAG, "onInitializationSuccess: provider is ${provider?.javaClass}")
+        Log.d(mTAG, "onInitializationSuccess: Youtube Player is ${youTubePlayer?.javaClass}")
+        Toast.makeText(this, "Initialized Youtube Player successfully", Toast.LENGTH_SHORT).show()
+
+        if (!wasRestored) {
+            youTubePlayer?.cueVideo(YOUTUBE_VIDEO_ID)
+        }
     }
 
-    override fun onInitializationFailure(p0: YouTubePlayer.Provider?, p1: YouTubeInitializationResult?) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    override fun onInitializationFailure(provider: YouTubePlayer.Provider?,
+                                         youTubeInitializationResult: YouTubeInitializationResult?) {
+        val REQUEST_CODE = 0
+        if (youTubeInitializationResult?.isUserRecoverableError == true) {
+            youTubeInitializationResult.getErrorDialog(this, REQUEST_CODE)
+        } else {
+            val errorMessage = "There was an error initializing the YoutubePlayer ($youTubeInitializationResult)"
+            Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show()
+        }
     }
 }
+
